00c7c5d1090b0853e36485e2594a9f90bc5edec4
KhanhTo <hungkhanh0709@gmail.com>
2025-06-28 11:13:30 +0700

    feat: update allowed domain

--- minna-no-hologram-bff/src/main/java/com/betonamura/hologram/config/WebConfig.java
+++ minna-no-hologram-bff/src/main/java/com/betonamura/hologram/config/WebConfig.java
@@ -13,7 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns(
+                        "http://localhost:3000",
+                        "https://minna-no-hologram-*.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
